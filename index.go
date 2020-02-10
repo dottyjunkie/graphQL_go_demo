9bbee278d07dc8cf56cf3356126577ac75dc1fb3
Dotty Junkie <dottyjunkie870407@gmail.com>
2020-02-10 13:38:12 +0800

    2020.02.10

---
+++ index.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	handler "github.com/graphql-go/graphql-go-handler"
+)
+
+func main() {
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "fuckboobs", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(rootQuery),
+	}
+	schema, _ := graphql.NewSchema(schemaConfig)
+
+	h := handler.New(&handler.Config{
+		Schema:   &schema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	http.Handle("/graphql", h)
+	http.ListenAndServe(":8080", nil)
+}
